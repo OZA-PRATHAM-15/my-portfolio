634c9367b96ccecdde98a87a19b0614843da64ae
Pratham Oza <pratham.oza10@gmail.com>
2025-07-05 18:32:58 +0530

    graphql get request edit

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,6 +1,7 @@
 import { createYoga, createSchema } from "graphql-yoga";
 import { typeDefs } from "@/graphql/schema";
 import { resolvers } from "@/graphql/resolvers";
+import { NextRequest } from "next/server";
 
 const yoga = createYoga({
   graphqlEndpoint: "/api/graphql",
@@ -8,6 +9,13 @@ const yoga = createYoga({
     typeDefs,
     resolvers,
   }),
+  fetchAPI: { Request, Response },
 });
 
-export { yoga as GET, yoga as POST };
+export async function GET(req: NextRequest) {
+  return yoga.handleRequest(req, {});
+}
+
+export async function POST(req: NextRequest) {
+  return yoga.handleRequest(req, {});
+}
